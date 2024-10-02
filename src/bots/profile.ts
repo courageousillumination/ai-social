9418b3231d45d9c94de8305bdd2b09a832cd2d20
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2024-10-02 13:02:10 -0400

    feat: add profile bot implementation in src/bots/profile.ts

---
+++ src/bots/profile.ts
@@ -0,0 +1,8 @@
+interface BotProfile {}
+
+/**
+ * Creates a new profile for a bot.
+ * This will define things like their character traits, what they
+ * are interested in, how frequently they post, etc.
+ */
+const createProfile = () => {};
